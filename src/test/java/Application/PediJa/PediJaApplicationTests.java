e07d605a64baaeb796801875121601d864b436d3
axthurdelfino <verdezera123@gmail.com>
2026-08-18 22:49:16 -0300

    PediJa

---
+++ src/test/java/Application/PediJa/PediJaApplicationTests.java
@@ -0,0 +1,13 @@
+package Application.PediJa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PediJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
