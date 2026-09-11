c813a1fc08f08383da03ad386433e8911c93956c
axthurdelfino <verdezera123@gmail.com>
2026-09-11 05:20:32 -0300

    Client Entity

---
+++ backend/src/main/java/Application/PediJa/Entities/Client.java
@@ -0,0 +1,102 @@
+package Application.PediJa.Entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+
+@Entity
+@Table(name = "cliente")
+public class Client {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String nome;
+
+  private String cpf;
+
+  private String telefone;
+
+  @Email
+  private String email;
+
+  private String endereco;
+
+  @Column(name = "data_cadastro")
+  private Instant dataCadastro;
+
+  public Client(){}
+
+  public Client(Long id, String nome, String cpf, String telefone, String email, String endereco, Instant dataCadastro) {
+    this.id = id;
+    this.nome = nome;
+    this.cpf = cpf;
+    this.telefone = telefone;
+    this.email = email;
+    this.endereco = endereco;
+    this.dataCadastro = dataCadastro;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getTelefone() {
+    return telefone;
+  }
+
+  public void setTelefone(String telefone) {
+    this.telefone = telefone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getEndereco() {
+    return endereco;
+  }
+
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
+
+  public Instant getDataCadastro() {
+    return dataCadastro;
+  }
+
+  public void setDataCadastro(Instant dataCadastro) {
+    this.dataCadastro = dataCadastro;
+  }
+}
