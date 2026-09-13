836ed554821d6395482abe5ec1ec41a0ee40e79a
axthurdelfino <verdezera123@gmail.com>
2026-09-13 08:58:44 -0300

    Product Service 1/2 implementation

--- backend/src/main/java/Application/PediJa/Exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/Application/PediJa/Exceptions/GlobalExceptionHandler.java
@@ -15,15 +15,21 @@ import jakarta.servlet.http.HttpServletRequest;
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(ResourceNotFoundException.class)
-  public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
-    StandardError error = new StandardError(Instant.now(), HttpStatus.NOT_FOUND.value(), "Resource not Found", e.getMessage(),
+  public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+
+    StandardError error = new StandardError(Instant.now(), HttpStatus.NOT_FOUND.value(), "Resource not Found",
+        e.getMessage(),
         request.getRequestURI());
+
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
   }
+
   @ExceptionHandler(BusinessException.class)
   public ResponseEntity<StandardError> businessException(BusinessException e, HttpServletRequest request) {
+
     StandardError error = new StandardError(Instant.now(), HttpStatus.UNPROCESSABLE_CONTENT.value(),
         "Business Exception", e.getMessage(), request.getRequestURI());
+
     return ResponseEntity.status(HttpStatus.UNPROCESSABLE_CONTENT).body(error);
   }
 
--- backend/src/main/java/Application/PediJa/services/ProductService.java
+++ backend/src/main/java/Application/PediJa/services/ProductService.java
@@ -1,9 +1,11 @@
 package Application.PediJa.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import Application.PediJa.Dto.RequestProduct;
 import Application.PediJa.Dto.ResponseProduct;
 import Application.PediJa.Entities.Product;
 import Application.PediJa.Exceptions.ResourceNotFoundException;
@@ -30,8 +32,16 @@ public class ProductService {
   }
 
   public ResponseProduct findById(Long id) {
-    Product product = productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product", id));
+    Optional<Product> result = productRepository.findById(id);
+    Product product = result.orElseThrow(() -> new ResourceNotFoundException("Product", id));
 
     return productMapper.toResponse(product);
+  }
+
+  public ResponseProduct insert(RequestProduct dto) {
+    Product pro = productMapper.toEntity(dto);
+    Product saved = productRepository.save(pro);
+
+    return productMapper.toResponse(saved);
   }
 }
