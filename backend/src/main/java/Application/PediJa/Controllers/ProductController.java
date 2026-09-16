bd33b2afd27d5b405f6b84cd00dc2f6163456a4b
axthurdelfino <verdezera123@gmail.com>
2026-09-16 06:31:15 -0300

    Product Controller

--- backend/src/main/java/Application/PediJa/Controllers/ProductController.java
+++ backend/src/main/java/Application/PediJa/Controllers/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,4 +52,10 @@ public class ProductController {
     return ResponseEntity.noContent().build();
   }
 
+  @PutMapping(value = "/{id}")
+  public ResponseEntity<ResponseProduct> update(@PathVariable Long id, @Valid @RequestBody RequestProduct request) {
+    ResponseProduct product = services.update(id, request);
+
+    return ResponseEntity.ok().body(product);
+  }
 }
