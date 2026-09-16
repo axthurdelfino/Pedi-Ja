7b68d038ac19dc45873640d13d01ad152c83d5d5
axthurdelfino <verdezera123@gmail.com>
2026-09-16 06:22:57 -0300

    Product Controller

--- backend/src/main/java/Application/PediJa/Controllers/ProductController.java
+++ backend/src/main/java/Application/PediJa/Controllers/ProductController.java
@@ -3,14 +3,19 @@ package Application.PediJa.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import Application.PediJa.Dto.RequestProduct;
 import Application.PediJa.Dto.ResponseProduct;
 import Application.PediJa.services.ProductService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -27,9 +32,19 @@ public class ProductController {
   }
 
   @GetMapping(value = "/{id}")
-  public ResponseEntity<ResponseProduct> findById(@PathVariable Long id){
+  @Valid
+  public ResponseEntity<ResponseProduct> findById(@PathVariable Long id) {
     ResponseProduct product = services.findById(id);
     return ResponseEntity.ok().body(product);
   }
+
+  @PostMapping
+  @Valid
+  public ResponseEntity<ResponseProduct> insert(@Valid @RequestBody RequestProduct obj) {
+    ResponseProduct product = services.insert(obj);
+    HttpStatus status = HttpStatus.CREATED;
+    return ResponseEntity.status(status).body(product);
+  }
+
 
 }
